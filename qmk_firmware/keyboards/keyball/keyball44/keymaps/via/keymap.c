df7dabd275bc06919ea487deb8f07347b8dc9f12
ngosuke <ngo.ksk.ppp@gmail.com>
2023-04-26 20:35:30 -0400

    separated function file for auto mouse layer. Disable hold functions

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "quantum.h"
-#include "my_functions.h"
+#include "features/my_functions.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
